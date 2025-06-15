cde9e66403cc476bdb6b0a67f547f5ce569cdd24
Andrew Baxter <>
2025-06-16 00:57:56 +0900

    Tweaking

--- pack.py
+++ pack.py
@@ -2,7 +2,7 @@ import pathlib
 import json
 
 here = pathlib.Path.cwd()
-out = here / "build"
+out = here / "pack"
 out.mkdir(exist_ok=True, parents=True)
 source = (here / "index.html").read_text()
 shaders = {}
